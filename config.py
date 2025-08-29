ae931fa56fdf49b767d54858afec0181985b7e4d
yassuhacks750 <yassudj8085231272@gmail.com>
2025-08-29 23:04:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,13 @@ import os
 ##Code Written By @ItsMeMaster
 
 class Config(object):
-    BOT_TOKEN = ""
+    BOT_TOKEN = "8434843795:AAHtx7x8Nvnfr-HS0tFyykCxXM_yyo_FgzA"
     DB_NAME = ""
-    API_ID = 
-    API_HASH = ""
-    ADMIN_ID = 
+    API_ID = 22849789
+    API_HASH = "0fc127c6055acd59f00ec6c229e1e3c4"
+    ADMIN_ID = 7296271316
     DB_URL = ""
     LOG_CHANNEL =  # Your Log Channel ID (Bot ko ADMIN BNAYE)
     USERLINK = ""
     TUTORIAL_VIDEO = ""
+
